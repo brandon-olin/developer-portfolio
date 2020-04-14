112c0dec13d2edd6f53109e59b6de2606ad2ae4d
brandon olin <olin.brandon@gmail.com>
2020-04-14 12:04:22 -0400

    commenting out JS code to fix heroku push errors

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -1,11 +1,11 @@
-import Typed from 'typed.js';
+// import Typed from 'typed.js';
 
-const loadDynamicBannerText = () => {
-  new Typed('#banner-typed-name', {
-    strings: ["Hi there!^600\nI'm Brandon Olin,^600\nRuby on Rails Developer"],
-    typeSpeed: 50,
-    loop: false
-  });
-}
+// const loadDynamicBannerText = () => {
+//   new Typed('#banner-typed-name', {
+//     strings: ["Hi there!^600\nI'm Brandon Olin,^600\nRuby on Rails Developer"],
+//     typeSpeed: 50,
+//     loop: false
+//   });
+// }
 
-export { loadDynamicBannerText };
+// export { loadDynamicBannerText };
