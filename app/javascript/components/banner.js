befedad4678ceb039170ddec1551da9cabf1291a
brandon olin <olin.brandon@gmail.com>
2020-05-08 10:56:02 -0400

    updated descriptions to reflect js proficiency and added smartbrain project to portfolio

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -2,7 +2,7 @@ import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
   new Typed('#banner-typed-name', {
-    strings: ["Hi there!^600\nI'm Brandon Olin,^600\nRuby on Rails Developer"],
+    strings: ["Hi there!^450\nI'm Brandon Olin,^450\nJavaScript Developer"],
     typeSpeed: 50,
     loop: false
   });
